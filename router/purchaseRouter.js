8819bd3d6a5c64810836ed2abc2798a2df82c649
Sujin Kang <1214sujin@naver.com>
2023-11-21 16:59:05 +0900

    manager view

--- router/purchaseRouter.js
+++ router/purchaseRouter.js
@@ -2,10 +2,14 @@ const express = require('express')
 var router = express.Router()
 
 var purchase = require('../lib/purchase')
+var cart = require('../lib/cart')
 
+// 구매내역
 router.get('/', (req, res) => {
+	if (req.session.class == '01') res.redirect('/purchase/view/v')
 	purchase.home(req, res)
 })
+
 router.post('/create', (req, res) => {
 	purchase.create_process(req, res)
 })
@@ -13,11 +17,40 @@ router.get('/cancel/:purId', (req, res) => {
 	purchase.cancel_process(req,res)
 })
 
-router.get('/cart', (req, res) => {
-	purchase.cart(req, res)
+router.get('/view/:vu', (req, res) => {
+	purchase.vu(req, res)
 })
+router.get('/create', (req, res) => {
+	purchase.create(req, res)
+})
+router.get('/update/purId', (req, res) => {
+	purchase.update(req, res)
+})
+router.post('/update_process', (req, res) => {
+	purchase.update_process(req, res)
+})
+
+// 장바구니
+router.get('/cart', (req, res) => {
+	if (req.session.class == '01') res.redirect('/purchase/cart/view/v')
+	cart.home(req, res)
+})
+
 router.post('/cart/create', (req, res) => {
-	purchase.cart_process(req, res)
+	cart.create_process(req, res)
+})
+
+router.get('/cart/view/:vu', (req, res) => {
+	cart.vu(req, res)
+})
+router.get('/cart/create', (req, res) => {
+	cart.create(req, res)
+})
+router.get('/cart/update/purId', (req, res) => {
+	cart.update(req, res)
+})
+router.post('/cart/update_process', (req, res) => {
+	cart.update_process(req, res)
 })
 
 module.exports = router
